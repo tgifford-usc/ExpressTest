11209378df8e8dceb1c99cf12d6b594a78f47851
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2024-10-07 20:27:15 +1000

    Added in local environment variables

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-  res.json({ name: "Xavier" });
+  res.json({ course: "DES222" });
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
